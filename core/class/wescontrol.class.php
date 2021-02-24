c4ee7a20f605f126f15daf3eb628af295c0efda3
sarakha63 <sarakha_ludovic@yahoo.fr>
2021-02-24 21:35:06 +0100

    add cronc reation if deleted manually

--- core/class/wescontrol.class.php
+++ core/class/wescontrol.class.php
@@ -168,7 +168,16 @@ class wescontrol extends eqLogic {
 		}
 		$cron = cron::byClassAndFunction(__CLASS__, 'daemon');
 		if (!is_object($cron)) {
-			throw new Exception(__('Tache cron introuvable', __FILE__));
+			if (!is_object($cron)) {
+				$cron = new cron();
+				$cron->setClass(__CLASS__);
+				$cron->setFunction('daemon');
+				$cron->setEnable(1);
+				$cron->setDeamon(1);
+				$cron->setTimeout(1440);
+				$cron->setSchedule('* * * * *');
+				$cron->save();
+			}
 		}
 		$cron->run();
 	}
